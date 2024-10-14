b19bb9482dd5abcab5433dd9261ad0caf1942b33
hanbyeol <jhb990438@gmail.com>
2024-10-14 21:26:17 +0900

    미로 탈출 추가

---
+++ src/main/java/Programmers/level_2/미로_탈출.java
@@ -0,0 +1,16 @@
+package Programmers.level_2;
+
+public class 미로_탈출 {
+
+    public static int solution() {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(solution());
+
+    }
+}
