084111a6d8136cbfc18573af122968984e85bacf
habo <jhb990438@gmail.com>
2025-01-14 14:59:14 +0900

    add

--- src/main/java/Backjoon/골드2/저울.java
+++ src/main/java/Backjoon/골드2/저울.java
@@ -6,6 +6,10 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/**
+ * 그리디로 풀어보기
+ */
+
 public class 저울 {
 
     public static void main(String[] args) throws IOException {
