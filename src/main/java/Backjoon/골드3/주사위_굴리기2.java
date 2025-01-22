1f25784a7968c0cff5c07b5cdc4afba7b16f4d53
habo <jhb990438@gmail.com>
2025-01-22 16:56:00 +0900

    add

---
+++ src/main/java/Backjoon/골드3/주사위_굴리기2.java
@@ -0,0 +1,40 @@
+package Backjoon.골드3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 주사위_굴리기2 {
+
+    static int N, M, K;
+    static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(Arrays.toString(map[i]));
+        }
+
+
+
+    }
+}
